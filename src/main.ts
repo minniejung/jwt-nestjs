4a3b192b9af20522fbe15f2dd0b685e354d2a908
minniejung <jungmignonne@gmail.com>
2025-05-14 13:51:51 +0900

    feat: add mock obj, postman result captures

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 9090);
 }
 
 bootstrap().catch((err) => {
-  console.error('앱 실행 중 오류 발생:', err);
+  console.error("앱 실행 중 오류 발생:", err);
 });
